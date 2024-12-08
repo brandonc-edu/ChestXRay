d57660bc27fee70af50b5439d417597165c0060a
Brandon Carrasco <carrascb@mcmaster.ca>
2024-12-08 13:46:33 -0500

    Fixed Bugs for Test.py
    Evaluation of model is now operational.

--- Test.py
+++ Test.py
@@ -1,9 +1,10 @@
-from tensorflow.keras.models import load_model
 import tensorflow as tf
-from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score, confusion_matrix
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 
-def evaluate_model(model, X, y, set_name='Test'):
+def evaluate_model(model, X, y, set_name='Test', labels=[0, 1, 2, 3], classes=["Covid19", "Normal", "Pneumonia", "Turberculosis"]):
     """
         Evaluates final model performance on given X & y set using a variety of metrics & graphical analyses.
 
@@ -18,35 +19,37 @@ def evaluate_model(model, X, y, set_name='Test'):
         - Confusion Matrix (TO BE FULLY IMPLEMENTED)
     """
     loss, accuracy = model.evaluate(X, y)
-    print(f"{set_name} Accuracy: {accuracy:.4f}")
+    print(f"{set_name} Accuracy: {accuracy * 100:.2f}%")
     print(f"{set_name} Loss: {loss:.4f}")
 
     # Get predictions
-    y_preds = model.predict(X)
-    y_true = y
+    y_preds = np.argmax(model.predict(X), axis=1)
+    y_true = np.argmax(y, axis=1)
 
     # Calculate metrics
-    precision = precision_score(y_true, y_preds, labels=["Boo"])
-    recall = recall_score(y_true, y_preds)
-    f1 = f1_score(y_true, y_preds)
+    precision = precision_score(y_true, y_preds, average='weighted', labels=labels) * 100
+    recall = recall_score(y_true, y_preds, average='weighted', labels=labels) * 100
+    f1 = f1_score(y_true, y_preds, average='weighted', labels=labels) * 100
     conf_matrix = confusion_matrix(y_true, y_preds)
 
     # Print metrics
-    print(f"Model {set_name} Precision: {precision:.4f}")
-    print(f"Model {set_name} Recall: {recall:.4f}")
-    print(f"Model {set_name} f1-score: {f1:.4f}")
-    ## Below two lines will be phased out once plot_confusion_matrix() is finished.
+    print(f"Model {set_name} Precision: {precision:.2f}%")
+    print(f"Model {set_name} Recall: {recall:.2f}%")
+    print(f"Model {set_name} f1-score: {f1:.2f}%")
+    ## Below two lines may be phased out once plot_confusion_matrix() is finished.
     print(f"Confusion Matrix for {set_name} Set predictions:") 
     print(conf_matrix)
-    # plot_confusion_matrix(conf_matrix)
+    plot_confusion_matrix(y_true, y_preds, classes)
 
     # Plot ROC Curves
 
-def plot_confusion_matrix(conf_mat):
+def plot_confusion_matrix(y_true, y_preds, classes=None):
     """
-        Plots the confusion matrix given confusion matrix.
+        Plots the confusion matrix given true values & predictions.
     """
-    pass
+    # Plots a simple confusion matrix using the true values, predicted values, and (if provided) class labels.
+    ConfusionMatrixDisplay.from_predictions(y_true, y_preds, display_labels=classes)
+    plt.show()
 
 def load_saved_model(filename='chest_xray_model.pkl'):
     with open(filename, 'rb') as file:
@@ -56,6 +59,7 @@ def load_saved_model(filename='chest_xray_model.pkl'):
 
 def main():
     # Load Test Data
+    X_test, y_test = np.load('X_test.npy'), np.load('Y_test.npy')
 
     # Load the saved model
     loaded_model = load_saved_model('chest_xray_model.pkl')
